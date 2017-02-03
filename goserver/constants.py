306962c7c4397030008e5c386cd08e05daf68e80
huayunfly <cuiguoxia@sohu.com>
2017-02-03 14:45:12 +0800

    Add enum constants.

---
+++ goserver/constants.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+"""
+Constants definition for GoIoT server.
+"""
+
+from enum import Enum
+
+__author__ = 'Yun Hua'
+__email__ = 'huayunflys@126.com'
+__url__ = 'https://github.com/huayunfly/goiot'
+__license__ = 'Apache License, Version 2.0'
+__version__ = '0.1'
+__status__ = 'Beta'
+
+
+class GoStatus(Enum):
+    """
+    Status codes.
+    """
+    S_OK = 0
+    S_ERROR = 1
+    S_INVALID_NAME = 2
+    S_INVALID_ID = 3
+    S_UNKNOWN = 4
+
+
+class GoQuality(Enum):
+    """
+    Quality codes.
+    """
+    Q_NORMAL = 1
+
+
+class GoOperation(Enum):
+    """
+    Operation codes.
+    """
+    OP_READ = 1
+    OP_WRITE = 2
+    OP_ASYNC_READ = 3
+    OP_ASYNC_WRITE = 4
+    OP_REFRESH = 5
+
+
+class GoPrivilege(Enum):
+    """
+    Privilege
+    """
+    READABLE = 1 << 0
+    WRITEABLE = 1 << 1
+
+
+GO_EU_TYPE_ANALOG = 0x1
+
+
