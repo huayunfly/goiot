e446ef2032099d2edb3e9bf7d602941f72676a3e
huayunfly <cuiguoxia@sohu.com>
2017-03-03 00:43:18 +0800

    Add test_write without csrf.

--- test/webapi_test.py
+++ test/webapi_test.py
@@ -13,6 +13,7 @@ class WebAPITest(unittest.TestCase):
     """
 
     def setUp(self):
+        self.main_url = 'http://127.0.0.1:8000'
         self.tags_rw_url = 'http://127.0.0.1:8000/da/api/tags'
         self.token = 'abc'
         self.key = {'data': [{'key': 1}, {'key': 2}, {'key': 3}, {'key': 4}]}
@@ -32,6 +33,22 @@ class WebAPITest(unittest.TestCase):
         r = requests.get(self.tags_rw_url, params=payload)
         self.assertEqual(r.status_code, 200)
         print(r.json())
+
+    def test_write_some(self):
+        """
+        Test write without (Django) csrf.
+        """
+        client = requests.session()
+
+        # Retrieve the CSRF token first
+        client.get(self.main_url)  # sets cookie
+        # csrftoken = client.cookies['csrftoken']
+
+        data = {'data': [{'key': 2, 'value': 88.8}]}
+        payload = dict(token=self.token, data=json.dumps(data))
+        # headers = {'Content-type': 'application/json', "X-CSRFToken": csrftoken, "Referer": URL}
+        r = requests.post(self.tags_rw_url, data=payload)
+        self.assertEqual(r.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
