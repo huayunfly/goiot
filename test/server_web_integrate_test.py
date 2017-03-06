033df5236cabd24de15a152d342b8663ab3c9929
huayunfly <cuiguoxia@sohu.com>
2017-03-06 17:28:38 +0800

    Add refresh_worker for client, solving data_changed() threading block.

--- test/server_web_integrate_test.py
+++ test/server_web_integrate_test.py
@@ -19,6 +19,7 @@ from device import deviceobj, honeywelludc
 REFRESH_RATE = 0.5  # in second
 WRITE_RATE = 0.5  # in second
 
+
 class WebApiClient(serviceclient.ServiceClient):
     """
     Class access Web APIs.
@@ -36,8 +37,12 @@ class WebApiClient(serviceclient.ServiceClient):
         self.tick = time.time()
         self.keep_working = False
         self.threads = []
+        self.rw_lock = threading.Lock()
         self.trans_id = 101
         # self.key = {'data': [{'key': 1}, {'key': 2}, {'key': 3}, {'key': 4}]}
+        self.post_tag_ids = []
+        self.post_values = []
+        self.post_results = []
 
     def start(self):
         self.tag_ids = self.service.browse_tag_id(self.tag_names)
@@ -45,6 +50,7 @@ class WebApiClient(serviceclient.ServiceClient):
         self.service.subscribe(self.name, self.data_changed, subscribe_tag_names)
         self.keep_working = True
         self.threads.append(threading.Thread(target=self.write_worker))
+        self.threads.append(threading.Thread(target=self.refresh_worker))
         for thread in self.threads:
             thread.start()
 
@@ -56,36 +62,14 @@ class WebApiClient(serviceclient.ServiceClient):
         self.service.unsubscribe(self.name)
 
     def data_changed(self, tag_ids, values, op_results):
-        if time.time() - self.tick < REFRESH_RATE:
-            return
+        """
+        Call back function for the service subscribe.
 
-        self.tick = time.time()
-        data = {'data': []}
-        for tag_id, raw_value, op_result in zip(tag_ids, values, op_results):
-            if GoStatus.S_OK != op_result:
-                continue
-
-            if isinstance(raw_value, float):
-                value = raw_value
-            elif isinstance(raw_value, int):
-                value = float(raw_value)
-            elif isinstance(raw_value, str):
-                value = float(hash(raw_value))
-            elif isinstance(raw_value, bool):
-                value = float(raw_value)
-            elif raw_value is None:
-                value = -27648.0
-            else:
-                assert False
-            key = self.keys[self.tag_ids.index(tag_id)]
-            data['data'].append(dict(key=key, value=value))
-        payload = dict(token=self.token, data=json.dumps(data))
-        try:
-            r = requests.post(self.tags_rw_url, data=payload)
-        except requests.exceptions.ConnectionError:
-            print('data_change(): connection failed.')
-            return
-        assert 200 == r.status_code
+        """
+        with self.rw_lock:
+            self.post_tag_ids = tag_ids[:]
+            self.post_values = values[:]
+            self.post_results = op_results[:]
 
     def read_completed(self, tag_ids, values, op_results, trans_id):
         raise NotImplementedError
@@ -93,7 +77,46 @@ class WebApiClient(serviceclient.ServiceClient):
     def write_completed(self, tag_ids, op_results, trans_id):
         pass
 
+    def refresh_worker(self):
+        """
+        Worker post the refreshing data to the web server.
+
+        """
+        while self.keep_working:
+            data = {'data': []}
+            with self.rw_lock:
+                for tag_id, raw_value, op_result in zip(self.post_tag_ids,
+                                                        self.post_values, self.post_results):
+                    if GoStatus.S_OK != op_result:
+                        continue
+
+                    if isinstance(raw_value, float):
+                        value = raw_value
+                    elif isinstance(raw_value, int):
+                        value = float(raw_value)
+                    elif isinstance(raw_value, str):
+                        value = float(hash(raw_value))
+                    elif isinstance(raw_value, bool):
+                        value = float(raw_value)
+                    elif raw_value is None:
+                        value = -27648.0
+                    else:
+                        assert False
+                    key = self.keys[self.tag_ids.index(tag_id)]
+                    data['data'].append(dict(key=key, value=value))
+            payload = dict(token=self.token, data=json.dumps(data))
+            try:
+                r = requests.post(self.tags_rw_url, data=payload)
+            except requests.exceptions.ConnectionError:
+                print('data_change(): connection failed.')
+            assert 200 == r.status_code
+            time.sleep(REFRESH_RATE)
+
     def write_worker(self):
+        """
+        Worker getting the setpoint data from web server and writing to the service.
+
+        """
         while self.keep_working:
             data = {'data': []}
             for key in self.request_keys:
