47ea715548ae790f99bafe971689ba066ce346b9
huayunfly <cuiguoxia@sohu.com>
2017-03-06 17:24:17 +0800

    Add MySQL option.

--- web/goiot/goiot/settings.py
+++ web/goiot/goiot/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '(odsd*sq4rbgq97!&^^f($a66uvx4dvt0zz+f(rix65^b-w0lc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.8.14']
 
 
 # Application definition
@@ -79,6 +79,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'goiot',
+        # 'USER': 'monitor',
+        # 'PASSWORD': '',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '3306',
     }
 }
 
