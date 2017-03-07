db89537bf7c864447bef7d71ca3bca3c8cdeaae5
huayunfly <huayunfly@126.com>
2017-03-07 17:09:12 +0800

    Add minimal_interval_after_write_sp in write_sp().

--- test/modbus_USB_test.py
+++ test/modbus_USB_test.py
@@ -1,9 +1,11 @@
 # Unit test for modbus RTU via RS-485, using FDTI USB convertor
 
+import time
 import unittest
 import minimalmodbus
 
 __author__ = "Yun Hua"
+
 
 class ModbusUSB4UDCTest(unittest.TestCase):
     """
@@ -18,20 +20,25 @@ class ModbusUSB4UDCTest(unittest.TestCase):
         self.instrument.handle_local_echo = False
         self.instrument.debug = False
         self.close_port_after_each_call = False
+        self.minimal_interval_after_write_sp = 0.21
 
     def tearDown(self):
-        self.instrument.serial.close()
+        #self.instrument.serial.close()
+        pass
 
     def test_read_pv(self):
-        for i in range(100):
+        for i in range(50):
             temperature = self.instrument.read_float(
                 self.register_pv, functioncode=4, numberOfRegisters=2)
             print('Current temperature: {}'.format(temperature))
 
     def test_write_sp(self):
-        with self.assertRaises(OSError) as e:
-            self.instrument.write_float(self.register_sp, 120.0)
-        print('OSError: {}'.format(e.exception))
+        for i in range(10):
+            self.instrument.write_float(self.register_sp, float(i))
+            # After set UDC SP, a minimal interval must be waited
+            # before the next command send (Read PV or Write SP),
+            # for the instrument needs time to handle setting SP.
+            time.sleep(self.minimal_interval_after_write_sp)
 
 
 if __name__ == '__main__':
