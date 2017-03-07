c644c2bf58a3dee6a3c43eff629d40ee472a4633
huayunfly <huayunfly@126.com>
2017-03-07 17:27:28 +0800

    Wait (minimal_interval_after_setpoint) after setting value.

--- device/honeywelludc.py
+++ device/honeywelludc.py
@@ -36,6 +36,11 @@ class HoneywellUDC(DeviceBase):
 
         self.port = port
         self.address = address
+        # Minimal wait time after setting the instrument value, before other commands(r/w)
+        # Instrument needs time to dealing with SV operation, while refusing any new command!
+        self.minimal_interval_after_setvalue = 0.25
+        # General read interval, 0s is OK
+        self.read_interval = 0.2
         self.registers_r = [0x40]
         self.registers_w = [0x78]
         self.invalid_value = -27648.0
@@ -84,7 +89,7 @@ class HoneywellUDC(DeviceBase):
                                GoOperation.OP_REFRESH, op_results, GoTransactionId.ID_EMPTY)
 
             self.out_queue.put(flow_var)
-            time.sleep(0.5)
+            time.sleep(self.read_interval)
 
     def internal_write(self, flow_var):
         results = []
@@ -97,12 +102,15 @@ class HoneywellUDC(DeviceBase):
                     if __debug__:
                         print('UDC write float value error')
                     results.append(GoStatus.S_COMM_ERROR)
+                    time.sleep(self.minimal_interval_after_setvalue)
                 except OSError:
                     if __debug__:
                         print('UDC write float os error')
                     results.append(GoStatus.S_COMM_ERROR)
+                    time.sleep(self.minimal_interval_after_setvalue)
                 else:
                     results.append(GoStatus.S_OK)
+                    time.sleep(self.minimal_interval_after_setvalue)
         flow_var.values = None
         flow_var.op_results = results
         flow_var.op_mode = GoOperation.OP_ASYNC_WRITE
