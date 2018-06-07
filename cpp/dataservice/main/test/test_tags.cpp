79a8053517ce5d8ab5df219f8067574b812ebb4d
huayun <huayunfly@126.com>
2018-06-07 16:50:49 +0800

    Update the project files

--- cpp/dataservice/main/test/test_tags.cpp
+++ cpp/dataservice/main/test/test_tags.cpp
@@ -8,10 +8,16 @@
 #define BOOST_TEST_MODULE testtags
 #include <boost/test/included/unit_test.hpp>
 
+#include <ctime>
+#include "../src/tagdef.h"
+
+using namespace goiot;
+
 BOOST_AUTO_TEST_SUITE(testtags)
 
-BOOST_AUTO_TEST_CASE(test_tag_structure)
+BOOST_AUTO_TEST_CASE(test_tag_def)
 {
+    TagState state;
     BOOST_CHECK(1 == 1);
 }
 
