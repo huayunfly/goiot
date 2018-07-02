424436fc76157d848ac3e388bbb36138744de510
huayun <huayunfly@126.com>
2018-07-02 17:30:11 +0800

    Add buffer and a test.

--- cpp/dataservice/main/src/buffer.h
+++ cpp/dataservice/main/src/buffer.h
@@ -6,6 +6,8 @@
  */
 
 #include <vector>
+#include <mutex>
+#include <condition_variable>
 
 namespace goiot
 {
@@ -13,42 +15,64 @@ namespace goiot
 template <typename T>
 class CircularBuffer
 {
-  public:
-    explicit CircularBuffer(std::size_t size) : mysize(size), inpos(0),
-                                                outpos(0), totalitems(0)
-    {
-        buffer.resize(size);
-    }
+public:
+  explicit CircularBuffer(std::size_t size) : mysize(size), inpos(0),
+                                              outpos(0), totalitems(0), doneflag(0)
+  {
+    buffer.resize(size);
+  }
 
-    ~CircularBuffer()
-    {
-    }
+  ~CircularBuffer()
+  {
+  }
 
-    /**
+  /**
      * Get an item.
      * @param item: an buffer item
+     * @throw std::system_error
      * 
-     * @return 0 if it is succeeded, otherwise the error code.
+     * @return 0 if it is succeeded, otherwise the error no.
+     * ECANCELED if it detects completion.
      */
-    int GetItem(T &item);
+  int GetItem(T &item);
 
-    /**
+  /**
      * Put an item.
      * @param item: an buffer item
+     * @throw std::system_error
      * 
-     * @return 0 if it is succeeded, otherwise the error code.
+     * @return 0 if it is succeeded, otherwise the error no.
+     * ECANCELED if it detects completion.
      */
-    int PutItem(const T &item);
+  int PutItem(const T &item);
 
-  private:
-    CircularBuffer(const CircularBuffer &);
-    const CircularBuffer &operator=(const CircularBuffer &);
+  /**
+   * Get the completion status
+   * @param flag: 0, it continues. otherwise 1, it completes
+   * @return 0 if it is succeeded, otherwise it failed.
+   */
+  int GetDone(int &flag);
 
-  private:
-    std::size_t mysize;
-    std::size_t inpos;
-    std::size_t outpos;
-    std::size_t totalitems;
-    std::vector<T> buffer;
+  /**
+   * Set the completion
+   * @return 0 if it is succeeded. otherwise it failed.
+   */
+  int SetDone(void);
+
+private:
+  CircularBuffer(const CircularBuffer &);
+  const CircularBuffer &operator=(const CircularBuffer &);
+
+private:
+  std::size_t mysize;
+  std::size_t inpos;
+  std::size_t outpos;
+  std::size_t totalitems;
+  int doneflag;
+  std::vector<T> buffer;
+
+  std::mutex bufferlock;
+  std::condition_variable items;
+  std::condition_variable slots;
 };
 } // namespace goiot
---
+++ cpp/dataservice/main/test/test_buffer.cpp
@@ -0,0 +1,109 @@
+/**
+ * Test circular buffer.
+ * 
+ * @author Yun Hua
+ * @version 0.1 2018.07.02
+ */
+
+#define BOOST_TEST_MODULE testbuffer
+#include <boost/test/included/unit_test.hpp>
+
+#include <ctime>
+#include <system_error>
+#include <future>
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include "../src/buffer.h"
+
+using namespace goiot;
+
+static CircularBuffer<double> &getbuffer()
+{
+    static CircularBuffer<double> buffer(1 << 5);
+    return buffer;
+}
+
+static void *producer(void *arg)
+{
+    int localdone = 0;
+    while (localdone)
+    {
+        try
+        {
+            getbuffer().PutItem(1.1);
+            getbuffer().GetDone(localdone);
+        }
+        catch (const std::system_error &e)
+        {
+            std::cerr << e.what() << std::endl;
+            return nullptr;
+        }
+    }
+    return nullptr;
+}
+
+static void *consumer(void *arg)
+{
+    double value = 0.0;
+    while (true)
+    {
+        try
+        {
+            getbuffer().GetItem(value);
+            std::cout << value << std::endl;
+        }
+        catch (const std::system_error &e)
+        {
+            std::cerr << e.what() << std::endl;
+            return nullptr;
+        }
+    }
+    return nullptr;
+}
+
+BOOST_AUTO_TEST_SUITE(testbuffer)
+
+BOOST_AUTO_TEST_CASE(test_buffer)
+{
+    int numberproducers = 3;
+    int numberconsumers = 3;
+    std::vector<std::future<void *>> result_producers;
+    std::vector<std::future<void *>> result_consumers;
+
+    std::future<void *> res1 = std::async(producer, (void *)nullptr);
+    std::future<void *> res2 = std::async(producer, (void *)nullptr);
+    std::future<void *> res3 = std::async(producer, (void *)nullptr);
+
+    std::future<void *> res4 = std::async(consumer, (void *)nullptr);
+    std::future<void *> res5 = std::async(consumer, (void *)nullptr);
+    std::future<void *> res6 = std::async(consumer, (void *)nullptr);
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    getbuffer().SetDone();
+    if (res1.wait_for(std::chrono::seconds(0)) != std::future_status::deferred ||
+    res2.wait_for(std::chrono::seconds(0)) != std::future_status::deferred ||
+    res3.wait_for(std::chrono::seconds(0)) != std::future_status::deferred ||
+    res4.wait_for(std::chrono::seconds(0)) != std::future_status::deferred ||
+    res5.wait_for(std::chrono::seconds(0)) != std::future_status::deferred ||
+    res6.wait_for(std::chrono::seconds(0)) != std::future_status::deferred)
+    {
+        while(res1.wait_for(std::chrono::seconds(0)) != std::future_status::ready &&
+        res2.wait_for(std::chrono::seconds(0)) != std::future_status::ready &&
+        res3.wait_for(std::chrono::seconds(0)) != std::future_status::ready &&
+        res4.wait_for(std::chrono::seconds(0)) != std::future_status::ready &&
+        res5.wait_for(std::chrono::seconds(0)) != std::future_status::ready &&
+        res6.wait_for(std::chrono::seconds(0)) != std::future_status::ready)
+        {
+            std::this_thread::yield();
+        }
+    }
+    res1.get();
+    res2.get();
+    res3.get();
+    res4.get();
+    res5.get();
+    res6.get();
+}
+
+BOOST_AUTO_TEST_SUITE_END()
