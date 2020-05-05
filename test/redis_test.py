8505b06ad44cd54e0e9adbb9a1aeddbeea8ae680
huayun <huayunfly@126.com>
2020-05-05 21:12:02 +0800

    update modbus_rtu json address.

---
+++ test/redis_test.py
@@ -0,0 +1,23 @@
+# Unit test for redis
+
+__author__ = 'Yun Hua'
+__email__ = 'huayunflys@126.com'
+__url__ = 'https://github.com/huayunfly/goiot'
+__license__ = 'Apache License, Version 2.0'
+__version__ = '0.1'
+__status__ = 'Beta'
+
+import time
+import unittest
+
+class RedisTest(unittest.TestCase):
+    """
+    Unit tests for redis, including the dictionary read and write
+    """
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    
