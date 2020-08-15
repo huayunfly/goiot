153ad3c951cf83158bb86c9e8161b3af1c504bf8
huayun <huayunfly@126.com>
2020-08-15 23:29:23 +0800

    Add

---
+++ cpp/8H82600/data_manager.cpp
@@ -0,0 +1,6 @@
+#include "data_manager.h"
+
+DataManager::DataManager()
+{
+
+}
---
+++ cpp/8H82600/data_manager.h
@@ -0,0 +1,12 @@
+#ifndef DATAMANAGER_H
+#define DATAMANAGER_H
+
+#include <QObject>
+
+class DataManager
+{
+public:
+    DataManager();
+};
+
+#endif // DATAMANAGER_H
