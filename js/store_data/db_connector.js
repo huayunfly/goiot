f5514f47e8f0f8f8fa1682a0996f9a27821c0020
yun_hua <yun_hua@FILESERVER3>
2020-10-20 20:07:46 +0800

    Write data to Postgresql

--- js/store_data/db_connector.js
+++ js/store_data/db_connector.js
@@ -3,8 +3,6 @@
 const redis = require('redis');
 const pg = require('pg');
 const assert = require('assert');
-const { SSL_OP_EPHEMERAL_RSA } = require('constants');
-const { runInThisContext } = require('vm');
 
 class DBConnector {
     constructor(connection_path) {
@@ -22,11 +20,12 @@ class DBConnector {
 }
 
 class RedisConnector extends DBConnector {
-    constructor(connection_path, data_config) {
+    constructor(connection_path, data_config, sqldb) {
         super(connection_path);
         this.data_config_ = data_config;
         this.keep_running_ = true;
         this.model_ = [];
+        this.sqldb_ = sqldb;
         this.open();
     }
 
@@ -44,8 +43,11 @@ class RedisConnector extends DBConnector {
         // the Redis client will attempt to retry the connection.
         this.db_.on('connect', () => console.log('Redis client connected to server.'));
 
-        this.db_.on('ready', () => { console.log('Redis server is ready.'), this.refresh_data() });
-        //  If an error event is fired and no error handler is attached, 
+        this.db_.on('ready', () => { 
+            console.log('Redis server is ready.');
+            setInterval(this.refresh_data, 5000, this);
+        });
+        // If an error event is fired and no error handler is attached, 
         // the application process will throw the error and crash.
         this.db_.on('error', err => console.error('Redis error', err));
     }
@@ -72,18 +74,24 @@ class RedisConnector extends DBConnector {
             }
             this.model_.push({driver_id: driver_id, data_array: data_array });
         }
+        this.sqldb_.create_tables(this.model_);
     }
 
-    refresh_data() {
-        for (const table of this.model_) {
-            const driver_id = table.driver_id;
-            // Batch is a Multi without transaction.
-            const batch = this.db_.batch();
+    refresh_data(obj) {
+        for (const table of obj.model_) {
+            const table_name = table.driver_id;
+            // Batch is a kind of Multi without transaction.
+            const batch = obj.db_.batch();
             for (const data of table.data_array) {
                 batch.hmget('refresh:' + data, 'value', 'result');
             }
             batch.exec((err, replies) => {
-                // To SQL
+                if (err) {
+                    console.err('Referesh data from redis failed.');
+                    return;
+                }
+                // Value is valid if Result is '0'. 
+                obj.sqldb_.insert(table_name, table.data_array, replies.map(n => Number(n[1]) == 0 ? n[0] : 'NULL'));
             });
         }
     }
@@ -92,20 +100,72 @@ class RedisConnector extends DBConnector {
 class PostgreSQLConnector extends DBConnector {
     constructor(connection_path) {
         super(connection_path);
+        this.table_created_ = false;
         this.open();
     }
 
+    // Connect to DB.
     open() {
         const vars = this.connection_path_.split(':');
         const host = vars[0];
         const port = Number(vars[1]);
         const database = vars[2];
-        this.db_ = new pg.Client({ host: host, port: port, database: database });
+        const user = vars[3];
+        const password = vars[4];
+        this.db_ = new pg.Client({ host: host, port: port, 
+            database: database, user: user, password: password });
         this.db_.connect((err) => {
             if (err) {
                 throw err;
             }
-            console.log('Connected to database', database);
+            console.log('PostgreSQL client connected to database', database);
+        });
+    }
+
+    close() {
+        if (!this.db_) {
+            this.db_.close();
+        }
+    }
+
+    // Create DB tables if not exist.
+    create_tables(model) {
+        if (!model) {
+            throw 'Model is null.';
+        }  
+        // Create table with special column name wrapped with "", like "mfcpfc.4.pv"    
+        for (const item of model) {
+            this.db_.query(`
+ CREATE TABLE IF NOT EXISTS ${item.driver_id}
+ (id SERIAL primary key, ${item.data_array.map(n => '"' + n + '"' + ' TEXT').join(',')}, 
+ time DOUBLE PRECISION, create_time TIMESTAMP WITH TIME ZONE not null default localtimestamp(0));
+ `, (err, result) => {
+                if (err) {
+                    throw err;
+                }
+                console.log(`Created table "${item.driver_id}"`);
+            });
+        }
+        this.table_created_ = true;
+    }
+
+    // Insert a new record.
+    insert(table, names, data_array) {
+        if (!this.table_created_) {
+            console.log(`Table "${table}" is not existed.`);
+            return;
+        }   
+        // Insert table with special column name wrapped with "", like "mfcpfc.4.pv"     
+        this.db_.query(`
+ INSERT INTO ${table} (${names.map(n => '"' + n + '"').join(',')}, time) VALUES (
+ ${data_array.map(n => '\'' + n.toString() + '\'').join(',')}, ${Date.now()/1000}
+ ) RETURNING id;
+`, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            const id = result.rows[0].id;
+            console.log('Inserted row with id %s', id);
         });
     }
 }
--- js/store_data/index.js
+++ js/store_data/index.js
@@ -5,6 +5,7 @@ const read_file = require('fs').readFile;
 const yargs = require('yargs');
 const RedisConnector = require('./db_connector').RedisConnector;
 const PostgreSQLConnector = require('./db_connector').PostgreSQLConnector;
+
 const argv = yargs.demandOption('f').nargs('f', 1).describe('f', 'JSON file to parse').argv;
 const file = argv.f;
 
@@ -19,8 +20,8 @@ read_file(argv.f, (err, data_buffer) => {
         try
         {
             const data_config = JSON.parse(data_buffer.toString());
-            const redis_connector = new RedisConnector('127.0.0.1:6379', data_config);
-            const postgre_connector = new PostgreSQLConnector('127.0.0.1:5432:goiot');
+            const postgresql = new PostgreSQLConnector('127.0.0.1:5432:goiot:postgres:hello@123');
+            const redis_connector = new RedisConnector('127.0.0.1:6379', data_config, postgresql);
         }
         catch (e)
         {
