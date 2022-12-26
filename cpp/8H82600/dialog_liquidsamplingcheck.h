c1051dafe1fc791c3d6c30361c64d0caaa68f4ca
huayun <huayunfly@126.com>
2022-12-26 09:47:18 +0800

    Commit DialogLiquidSamplingCheck.h

--- cpp/8H82600/dialog_liquidsamplingcheck.h
+++ cpp/8H82600/dialog_liquidsamplingcheck.h
@@ -12,11 +12,36 @@ class DialogLiquidSamplingCheck : public QDialog
     Q_OBJECT
 
 public:
-    explicit DialogLiquidSamplingCheck(QWidget *parent = nullptr);
+    enum class TaskCategory
+    {
+        SAMPLING = 0,
+        COLLECTION = 1
+    };
+
+public:
+    explicit DialogLiquidSamplingCheck(QWidget *parent = nullptr,
+                                       TaskCategory category = TaskCategory::SAMPLING);
     ~DialogLiquidSamplingCheck();
+
+private slots:
+    void on_checkBox_power_stateChanged(int arg1);
+
+    void on_checkBox_purge_stateChanged(int arg1);
+
+    void on_checkBox_sink_stateChanged(int arg1);
+
+    void on_checkBox_obstacle_stateChanged(int arg1);
+
+    void on_checkBox_light_stateChanged(int arg1);
+
+    void on_checkBox_exp_stop_stateChanged(int arg1);
+
+private:
+    void CheckStatus();
 
 private:
     Ui::DialogLiquidSamplingCheck *ui;
+    TaskCategory category_;
 };
 
 #endif // DIALOG_LIQUIDSAMPLINGCHECK_H
