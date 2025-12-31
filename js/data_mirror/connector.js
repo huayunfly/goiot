2454700109bc8a8f1c2b2974b8424eb47c61f257
hy <huayunfly@126.com>
2025-12-31 17:09:01 +0800

    data.mirror updates refresh_data().

--- js/data_mirror/connector.js
+++ js/data_mirror/connector.js
@@ -3,6 +3,7 @@ const zlib = require('zlib');
 const assert = require('assert');
 const redis = require('ioredis');
 const fetch = require('node-fetch');
+const { group } = require('console');
 
 class DBConnector {
     constructor(connection_path) {
@@ -16,6 +17,26 @@ class DBConnector {
 
     close() {
 
+    }
+}
+
+class SetDataR {
+    constructor(service_name, operation, token, group_name, data_list)
+    {
+        this.name = service_name;
+        this.operation = operation;
+        this.token = token;
+        this.group_name = group_name;
+        this.list = data_list;
+    }
+
+    tojson()
+    {
+        return {name: this.name,
+            operation: this.operation,
+            token: this.token,
+            data: {group_name: this.group_name, list: this.list}
+        };
     }
 }
 
@@ -60,19 +81,21 @@ class RedisConnector extends DBConnector {
         if (!this.data_config_) {
             throw 'Undefined data configuration.';
         }
-        const name = this.data_config_.name;
-        for (const driver of this.data_config_.drivers) {
+        const group_name = this.data_config_.name;
+        for (const driver of this.data_config_.drivers) 
+        {
             const driver_id = driver.id;
+            const grp_driver_id = [group_name, driver_id].join('.'); 
             const data_array = [];
             for (const node of driver.nodes) {
                 const address = node.address;
                 for (const data of node.data) {
                     if (!data.register.toUpperCase().startsWith('W')) {
-                        data_array.push([driver_id, address, data.id].join('.'));
+                        data_array.push([group_name, driver_id, address, data.id].join('.'));
                     }
                 }
             }
-            this.model_.push({ driver_id: driver_id, data_array: data_array });
+            this.model_.push({ grp_driver_id: grp_driver_id, data_array: data_array });
         }
     }
 
@@ -83,7 +106,7 @@ class RedisConnector extends DBConnector {
             // Pipeline is a kind of Multi without transaction.
             const pipeline = obj.db_.pipeline();
             for (const data of table.data_array) {
-                pipeline.hmget('refresh:' + data, 'value', 'result');
+                pipeline.hmget('refresh:' + data, 'value', 'result', 'timestamp');
             }
             const reply = await pipeline.exec((err, replies) => {
                 if (err) {
@@ -92,7 +115,10 @@ class RedisConnector extends DBConnector {
                 }
             });
             // Value is valid if Result is '0'.  ? n[0] : 'NULL'));
-            obj.webapi_.post('', {token: null, name: 'SetData', data: reply});
+            const data_list = [{"id":"mfcpfc.1.pv", "value":"11.0", "result":"0", "timestamp": 1677154222.821000},
+            {"id":"mfcpfc.2.pv", "value":"12.1", "result":"0", "timestamp": 1677154222.839000}];
+            const body = new SetDataR('service_name', 'SetDataR', '6ac89607254a437c90c28ccc1c034706', "goiot", data_list);
+            obj.webapi_.post('', body);
         }
     }
 }
@@ -107,35 +133,41 @@ class WebServiceConnector {
         this.api_path_ = api_path;
     }
 
-    post(service_path, data_obj) {
+    async post(service_path, data_obj) {
         const data = JSON.stringify(data_obj);
         const options = {
-            hostname: this.hostname_,
-            port: this.port_,
-            path: this.api_path_ + service_path,
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
+                'User-Agent': `nodejs/${process.version}`,
                 'Content-Encoding': 'gzip',
-            }
+            },
+            body: data
         };
 
-        zlib.gzip(data, (err, buffer) => {
-            const req = https.request(options, res => {
-                console.log(`状态码: ${res.statusCode}`);
-                res.setEncoding('utf8'); // Note: not requesting or handling compressed response
-                res.on('data', d => {
-                    // ... do stuff with returned data
-                })
-            });
+        // zlib.gzip(data, (err, buffer) => {
+        //     const req = https.request(options, res => {
+        //         console.log(`状态码: ${res.statusCode}`);
+        //         res.setEncoding('utf8'); // Note: not requesting or handling compressed response 
+        //         res.on('data', d => {
+        //             // ... do stuff with returned data
+        //         })
+        //     });
 
-            req.on('error', err => {
-                console.error('problem with request: ' + err.message);
-            });
+        //     req.on('error', err => {
+        //         console.error('problem with request: ' + err.message);
+        //     });
 
-            req.write(buffer);
-            req.end();
-        });
+        //     req.write(buffer);
+        //     req.end();
+        // });
+
+        // fetch with "compress:true" by default
+        (async () => {
+            const res = await fetch(this.api_path_ + service_path, options);
+            const payload = await res.json();
+            console.log(payload);
+        })();
     }
 }
 
