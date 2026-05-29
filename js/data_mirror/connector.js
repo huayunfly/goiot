7a2ae4bfd2d9cbce4b452cb91a247677657c9277
hy <huayunfly@126.com>
2026-05-29 15:02:37 +0800

    data_mirror corrects poll_data error by adding redis.ZADD call.

--- js/data_mirror/connector.js
+++ js/data_mirror/connector.js
@@ -6,6 +6,15 @@ const fetch = require('node-fetch');
 const { group } = require('console');
 const { exit } = require('process');
 
+
+const D_NAMESPACE =
+{
+    'NS_REFRESH': 'refresh:',
+    'NS_POLL': 'poll:',
+    'NS_REFRESH_TIME': 'time_r:',
+    'NS_POLL_TIME': 'time_p:',
+}
+
 class DBConnector {
     constructor(connection_path) {
         this.connection_path_ = connection_path;
@@ -22,13 +31,13 @@ class DBConnector {
 }
 
 class SetDataR {
-    constructor(service_name, operation, token, group_name, data_list)
+    constructor(service_name, operation, token, group_name, table)
     {
         this.name = service_name;
-        this.operation = operation;
+        this.operation = 'SetDataR';
         this.token = token;
         this.group_name = group_name;
-        this.list = data_list;
+        this.table = table;
     }
 
     toJSON()
@@ -36,7 +45,7 @@ class SetDataR {
         return {name: this.name,
             operation: this.operation,
             token: this.token,
-            data: {group_name: this.group_name, list: this.list}
+            data: {group_name: this.group_name, table: this.table}
         };
     }
 }
@@ -60,7 +69,7 @@ class GetDataP {
         return {name: this.name,
             operation: this.operation,
             token: this.token,
-            data: {group_name: this.group_name, id_list: this.id_list, time_range: this.time_range, 
+            condition: {group_name: this.group_name, id_list: this.id_list, time_range: this.time_range, 
                 properties: this.properties, batch_size: this.batch_size, batch_num: this.batch_num }
         };
     }
@@ -159,7 +168,7 @@ class RedisConnector extends DBConnector {
 
         this.db_.on('ready', () => {
             console.log('Redis server is ready.');
-            setInterval(this.refresh_data, 5000, this);
+            setInterval(this.refresh_data, 3000, this);
             setInterval(this.poll_data, 1000, this);
             setInterval(this.check_connection, 10000, this);
         });
@@ -180,17 +189,15 @@ class RedisConnector extends DBConnector {
         for (const driver of this.data_config_.drivers) 
         {
             const driver_id = driver.id;
-            const grp_driver_id = [group_name, driver_id].join('.'); 
             const id_array = [];
             const id_array_write = [];
             for (const node of driver.nodes) {
-                const address = node.address;
                 for (const data of node.data) {
-                    if (data.register.toUpperCase().includes('R')) {
-                        id_array.push([driver_id, address, data.id].join('.'));
+                    if (data.register.toUpperCase().startsWith('R')) {
+                        id_array.push([driver_id, node.address, data.id].join('.'));
                     }
-                    else if (data.register.toUpperCase().includes('W')) {
-                        id_array_write.push([driver_id, address, data.id].join('.'));
+                    if (data.register.toUpperCase().startsWith('RW') || data.register.toUpperCase().startsWith('W')) {
+                        id_array_write.push([driver_id, node.address, data.id].join('.'));
                     }
                 }
             }
@@ -207,7 +214,7 @@ class RedisConnector extends DBConnector {
             const pipeline = obj.db_.pipeline();
             for (const data_id of table.id_array) {
                 id_list.push(data_id);               
-                pipeline.hmget('refresh:' + data_id, 'value', 'result', 'time');
+                pipeline.hmget(D_NAMESPACE.NS_REFRESH + data_id, 'value', 'result', 'time');
             }
             const reply = await pipeline.exec((err, replies) => {
                 if (err) {
@@ -215,38 +222,36 @@ class RedisConnector extends DBConnector {
                     return;
                 }
             });
-            const data_list = [];
+            const data_table = {"id": [], "value": [], "result": [], "time": []};
             let id_no = 0;
             const t_last = Date.now() / 1000.0 - 15.0;
             for (const item of reply)
             {
                 let t_data = Number(item[1][2]);
                 if (!Number.isNaN(t_data) && t_data > t_last) {
-                    data_list.push({
-                        "id": id_list[id_no], "value": item[1][0],
-                        "result": item[1][1], "time": t_data
-                    });
+                    data_table.id.push(id_list[id_no]);
+                    data_table.value.push(item[1][0]);
+                    data_table.result.push(item[1][1]);
+                    data_table.time.push(t_data);
                 }
                 id_no++;
             }
 
-            const data_list1 = [{ "id": "mfcpfc.1.pv", "value": "11.0", "result": "0", "time": 1677154221.821000 },
-            { "id": "mfcpfc.2.pv", "value": "12.1", "result": "0", "time": 1677154221.839000 }];
-            if (data_list.length > 0 && obj.webapi_.token) {
-                const body = new SetDataR('service_name', 'SetDataR', obj.webapi_.token, table.grp_name, data_list);
+            if (data_table.id.length > 0 && obj.webapi_.token) {
+                const body = new SetDataR('service_name', 'SetDataR', obj.webapi_.token, table.grp_name, data_table);
                 obj.webapi_.post(body);
             }
         }
     }
 
     // Callback function for polling data from databus api.
-    async poll_data() {
-        for (const table of this.model_write_) {
+    async poll_data(obj) {
+        for (const table of obj.model_write_) {
             const id_list = [];
             for (const data_id of table.id_array) {
                 id_list.push(data_id);
             }
-            if (id_list.length > 0 && this.webapi_.token) {
+            if (id_list.length > 0 && obj.webapi_.token) {
                 let batch_count = Math.ceil(id_list.length / 128);
                 for (let batch_num = 0; batch_num < batch_count; batch_num++) {
                     // One batch = 128 items
@@ -255,8 +260,8 @@ class RedisConnector extends DBConnector {
                     const time_range = [time_last - 15.0, time_last];
                     const properties = ['value', 'result', 'time'];
                     // Call databus api to get data, using a constant batch size of 128 to control the request size.
-                    const body = new GetDataP('service_name', 'GetDataP', this.webapi_.token, table.grp_name, id_list_batch, time_range, properties, 128, 1);
-                    const response = await this.webapi_.post(body);
+                    const body = new GetDataP('service_name', 'GetDataP', obj.webapi_.token, table.grp_name, id_list_batch, time_range, properties, 128, 1);
+                    const response = await obj.webapi_.post(body);
                     if (response?.statusCode == '200' && response.result?.data?.table?.id?.length > 0) {
                         if (response.result.data.table.id.length != response.result.data.table.value.length ||
                             response.result.data.table.id.length != response.result.data.table.result.length ||
@@ -265,8 +270,10 @@ class RedisConnector extends DBConnector {
                             console.error('Data size mismatch.');
                             continue;
                         }
-                        const pipeline = this.db_.pipeline();
+                        let time_id_list = [];
+                        const pipeline = obj.db_.pipeline();
                         for (let i = 0; i < response.result.data.table.id.length; i++) {
+
                             const data_id = response.result.data.table.id[i];
                             const value = response.result.data.table.value[i];
                             const result = response.result.data.table.result[i];
@@ -275,7 +282,8 @@ class RedisConnector extends DBConnector {
                                 console.error('Invalid data found.');
                                 continue;
                             }
-                            pipeline.hmset('poll:' + data_id, 'value', value, 'result', result, 'time', time);
+                            pipeline.hmset(D_NAMESPACE.NS_POLL + data_id, 'value', value, 'result', result, 'time', time);
+                            time_id_list.push({time: time, id: data_id});
                         }
                         const reply = await pipeline.exec((err, replies) => {
                             if (err) {
@@ -283,6 +291,17 @@ class RedisConnector extends DBConnector {
                                 return;
                             }
                         });
+                        if (time_id_list.length > 0) {
+                            for (const item of time_id_list) {
+                                pipeline.zadd(D_NAMESPACE.NS_POLL_TIME, item.time, D_NAMESPACE.NS_POLL + item.id);
+                            }
+                            await pipeline.exec((err, replies) => {
+                                if (err) {
+                                    console.err('Write time index to redis failed.');
+                                    return;
+                                }                          
+                            });
+                        }
                     }
                 }
             }
